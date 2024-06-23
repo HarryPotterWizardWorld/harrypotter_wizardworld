3f581fea0aa29ea09945299f6d2ba6fea201f439
Leticia Nobre <leticiavidal2910@gmail.com>
2024-06-23 20:35:28 -0300

    refat: refact navbar structure

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,26 +17,16 @@ export default function Navbar () {
     }
     return (
         <Flex as="header" p='3'>
-      <Flex py="2" w='100%' px="8" alignItems="center" bg="rgba(150, 150, 150, 0.155)" borderRadius="12" border='none' backdropFilter="blur(10px)">
+      <Flex py="2" w='100%' px="8" alignItems="center" bg="rgba(100, 123, 200, 0.2)" borderRadius="12" border='none' backdropFilter="blur(10px)">
         <Text fontSize="xl" fontWeight="300" color="white">WIZARD WORLD</Text>
         <Spacer />
         <Flex justifyContent='end'>
-          <Button {...buttonHeader}>Shop All</Button>
           <Button {...buttonHeader}>
             <Link href="/About" passHref>
               About Us
             </Link>
           </Button>
           <Button {...buttonHeader}>New</Button>
-          <Menu isLazy>
-            <MenuButton {...buttonHeader} px='16px'>More</MenuButton>
-            <MenuList>
-              {/* MenuItems are not rendered unless Menu is open */}
-              <MenuItem bg='none'>New Window</MenuItem>
-              <MenuItem bg='none'>Open Closed Tab</MenuItem>
-              <MenuItem bg='none'>Open File</MenuItem>
-            </MenuList>
-          </Menu>
         </Flex>
       </Flex>
       </Flex>
